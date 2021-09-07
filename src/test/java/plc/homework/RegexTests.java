d115dcf5b8a8a232c56f116db9892637bcbf8a19
Jake Richard <jakerichard@ufl.edu>
2021-09-07 15:36:43 -0400

    9/7/21 Commit

--- src/test/java/plc/homework/RegexTests.java
+++ src/test/java/plc/homework/RegexTests.java
@@ -119,14 +119,15 @@ public class RegexTests {
                 Arguments.of("Long Mantissa", "3.141592655359", true),
                 Arguments.of("Long Both", "19473626483.47291284628", true),
                 Arguments.of("Less Than One", "0.125", true),
-                Arguments.of("Leading 0s", "002.125", true),
+                Arguments.of("Ending 0s", "2.12500", true),
                 Arguments.of("Negative", "-4.5", true),
 
                 Arguments.of("Multiple Decimals", "19.24.56", false),
                 Arguments.of("Non-digit Characters", "2637erf.48hd", false),
                 Arguments.of("Symbols", "!#@.657", false),
                 Arguments.of("Missing Mantissa", "923.", false),
-                Arguments.of("Missing Whole", ".38272", false)
+                Arguments.of("Missing Whole", ".38272", false),
+                Arguments.of("Leading 0s", "00125.678", false)
         );
     }
 
