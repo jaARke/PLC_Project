e82f857480e604657b2d901309ee5fb24f67a68d
Jake <jake.richard126@gmail.com>
2021-09-09 19:03:02 -0400

    9/9/21 Commit (2)

---
+++ src/main/java/plc/project/Lexer.java
@@ -0,0 +1,146 @@
+package plc.project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The lexer works through three main functions:
+ *
+ *  - {@link #lex()}, which repeatedly calls lexToken() and skips whitespace
+ *  - {@link #lexToken()}, which lexes the next token
+ *  - {@link CharStream}, which manages the state of the lexer and literals
+ *
+ * If the lexer fails to parse something (such as an unterminated string) you
+ * should throw a {@link ParseException} with an index at the character which is
+ * invalid.
+ *
+ * The {@link #peek(String...)} and {@link #match(String...)} functions are * helpers you need to use, they will make the implementation a lot easier. */
+public final class Lexer {
+
+    private final CharStream chars;
+
+    public Lexer(String input) {
+        chars = new CharStream(input);
+    }
+
+    /**
+     * Repeatedly lexes the input using {@link #lexToken()}, also skipping over
+     * whitespace where appropriate.
+     */
+    public List<Token> lex() {
+        while (chars.has(1)) {  // While there are characters in the data CharStream
+
+        }
+        return null;
+    }
+
+    /**
+     * This method determines the type of the next token, delegating to the
+     * appropriate lex method. As such, it is best for this method to not change
+     * the state of the char stream (thus, use peek not match).
+     *
+     * The next character should start a valid token since whitespace is handled
+     * by {@link #lex()}
+     */
+    public Token lexToken() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexIdentifier() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexNumber() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexCharacter() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexString() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public void lexEscape() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexOperator() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    /**
+     * Returns true if the next sequence of characters match the given patterns,
+     * which should be a regex. For example, {@code peek("a", "b", "c")} would
+     * return true if the next characters are {@code 'a', 'b', 'c'}.
+     */
+    public boolean peek(String... patterns) {   // Checks to see if the next character(s) match the pattern parameter
+        for (int i = 0; i < patterns.length; i++) { // Verifies that each character in chars matches the corresponding character in the pattern parameter
+            if (chars.has(i) && String.valueOf(chars.get(i)).matches(patterns[i])) {
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Returns true in the same way as {@link #peek(String...)}, but also
+     * advances the character stream past all matched characters if peek returns
+     * true. Hint - it's easiest to have this method simply call peek.
+     */
+    public boolean match(String... patterns) {
+        if (peek(patterns)) {   // If the upcoming character(s) match the pattern:
+            for (int i = 0; i < patterns.length; i++) { // Advance chars the length of the pattern -- the read characters can now be emitted
+                chars.advance();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * A helper class maintaining the input string, current index of the char
+     * stream, and the current length of the token being matched.
+     *
+     * You should rely on peek/match for state management in nearly all cases.
+     * The only field you need to access is {@link #index} for any {@link
+     * ParseException} which is thrown.
+     */
+    public static final class CharStream {
+
+        private final String input;
+        private int index = 0;
+        private int length = 0;
+
+        public CharStream(String input) {
+            this.input = input;
+        }
+
+        public boolean has(int offset) {
+            return index + offset < input.length();
+        }
+
+        public char get(int offset) {
+            return input.charAt(index + offset);
+        }
+
+        public void advance() {
+            index++;
+            length++;
+        }
+
+        public void skip() {
+            length = 0;
+        }
+
+        public Token emit(Token.Type type) {
+            int start = index - length;
+            skip();
+            return new Token(type, input.substring(start, index), start);
+        }
+
+    }
+
+}
