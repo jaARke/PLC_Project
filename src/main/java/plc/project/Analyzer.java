78f4b2bedc4adf2cfb2c8fdd8c07518dbfae0a37
Jake Richard <jake.richard126@gmail.com>
2021-11-13 16:00:00 -0500

    Final Analyzer test submission (36/36 tests passed)

--- src/main/java/plc/project/Analyzer.java
+++ src/main/java/plc/project/Analyzer.java
@@ -1,6 +1,6 @@
 package plc.project;
 
-import jdk.internal.org.objectweb.asm.tree.analysis.AnalyzerException;
+
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -17,7 +17,7 @@ import java.util.stream.Collectors;
 public final class Analyzer implements Ast.Visitor<Void> {
 
     public Scope scope;
-    private Ast.Function function;
+    private Environment.Type funcRet;   // Used to store the return type of functions undergoing analysis
 
     public Analyzer(Scope parent) {
         scope = new Scope(parent);
@@ -33,27 +33,25 @@ public final class Analyzer implements Ast.Visitor<Void> {
         // Visit globals, followed by functions:
         ast.getGlobals().forEach(this::visit);
         ast.getFunctions().forEach(this::visit);
-        // Check for exception cases:
-        Environment.Function mainFunc = scope.lookupFunction("main", 0);
-        if (!mainFunc.getReturnType().getName().equals("Integer")) {
+        // Check for the exception condition:
+        if (!scope.lookupFunction("main", 0).getReturnType().equals(Environment.Type.INTEGER)) {
             throw new RuntimeException("Main method must have an integer return type.");
         }
+
         return null;
     }
 
     @Override
     public Void visit(Ast.Global ast) {
-        // If the value is present, visit it:
+        // If the value is present, visit it. Then check that it is assignable to the variable:
         if (ast.getValue().isPresent()) {
             visit(ast.getValue().get());
+            requireAssignable(Environment.getType(ast.getTypeName()), ast.getValue().get().getType());
         }
-        // Create the variable object and check that it meets assignable conditions:
-        Environment.Variable var = new Environment.Variable(ast.getName(), ast.getName(), Environment.getType(ast.getTypeName()),
-                ast.getMutable(), Environment.NIL);
-        requireAssignable(var.getType(), var.getValue().getType());
-        // Set the variable in the AST and define it in the current scope:
-        ast.setVariable(var);
-        scope.defineVariable(var.getName(), var.getJvmName(), var.getType(), var.getMutable(), var.getValue());
+        // Define the variable in the current scope:
+        scope.defineVariable(ast.getName(), ast.getName(), Environment.getType(ast.getTypeName()), ast.getMutable(), Environment.NIL);
+        // Set the variable in the AST:
+        ast.setVariable(scope.lookupVariable(ast.getName()));
 
         return null;
     }
@@ -65,18 +63,16 @@ public final class Analyzer implements Ast.Visitor<Void> {
         for (String i : ast.getParameterTypeNames()) {
             paramTypes.add(Environment.getType(i));
         }
-        // Construct the function object
-        Environment.Function func = new Environment.Function(ast.getName(), ast.getName(), paramTypes,
-                Environment.getType(ast.getReturnTypeName().orElse("Nil")), args -> Environment.NIL);
-        // Set the function in the AST and define it in the current scope:
-        ast.setFunction(func);
-        scope.defineFunction(func.getName(), func.getJvmName(), paramTypes, func.getReturnType(), args -> Environment.NIL);
-        // Define a new scope, store the function node in a variable (to access return type in return visit method), then visit each statement in the function:
+        // Define the function in the current scope:
+        scope.defineFunction(ast.getName(), ast.getName(), paramTypes, Environment.getType(ast.getReturnTypeName().orElse("Nil")), args -> Environment.NIL);
+        // Set the function in the AST:
+        ast.setFunction(scope.lookupFunction(ast.getName(), ast.getParameters().size()));
+        // Define a new scope, store the function's return type in a variable (to be used by visit(Ast.Statement.Return)), then visit each statement in the function:
         scope = new Scope(scope);
-        function = ast;
+        funcRet = Environment.getType(ast.getReturnTypeName().orElse("Nil"));
         ast.getStatements().forEach(this::visit);
-        // Restore function to null and scope to parent:
-        function = null;
+        // Restore funcRet to null and scope to parent:
+        funcRet = null;
         scope = scope.getParent();
 
         return null;
@@ -84,110 +80,330 @@ public final class Analyzer implements Ast.Visitor<Void> {
 
     @Override
     public Void visit(Ast.Statement.Expression ast) {
+        // Check for the exception condition:
         if (!(ast.getExpression() instanceof Ast.Expression.Function)) {
             throw new RuntimeException("Expected a function expression.");
         }
+        // Visit the function expression:
+        visit(ast.getExpression());
+
         return null;
     }
 
     @Override
     public Void visit(Ast.Statement.Declaration ast) {
-        // Determine the type of the variable being declared:
+        // Determine the type of the variable being declared and check exception conditions:
         Environment.Type type;
         if (!(ast.getTypeName().isPresent())) {
             if (!(ast.getValue().isPresent())) {
                 throw new RuntimeException("Type of declared variable could not be discerned.");
             }
+            visit(ast.getValue().get());
             type = ast.getValue().get().getType();
         }
         else {
             type = Environment.getType(ast.getTypeName().get());
+            if (ast.getValue().isPresent()) {
+                visit(ast.getValue().get());
+                requireAssignable(type, ast.getValue().get().getType());
+            }
         }
-        // Visit the value (if present) and check the exception condition that the variable type and value type are not compatible:
-        if (ast.getValue().isPresent()) {
-            visit(ast.getValue().get());
-            requireAssignable(type, ast.getValue().get().getType());
-        }
-        // Declare the variable in the current scope:
+        // Define the variable in the current scope:
         scope.defineVariable(ast.getName(), ast.getName(), type, true, Environment.NIL);
+        // Set the variable in the AST:
+        ast.setVariable(scope.lookupVariable(ast.getName()));
 
         return null;
     }
 
     @Override
     public Void visit(Ast.Statement.Assignment ast) {
-        throw new UnsupportedOperationException();  // TODO
+        // Check the exception condition:
+        if (!(ast.getReceiver() instanceof Ast.Expression.Access)) {
+            throw new RuntimeException("Invalid assignment operation.");
+        }
+        // Visit both sides of the assignment expression:
+        visit(ast.getReceiver());
+        visit(ast.getValue());
+        // Ensure the right side is assignable to the left side:
+        requireAssignable(ast.getReceiver().getType(), ast.getValue().getType());
+
+        return null;
     }
 
     @Override
     public Void visit(Ast.Statement.If ast) {
-        throw new UnsupportedOperationException();  // TODO
+        // Visit the condition:
+        visit(ast.getCondition());
+        // Check both exception conditions:
+        if (!ast.getCondition().getType().equals(Environment.Type.BOOLEAN)) {
+            throw new RuntimeException("Invalid condition in IF statement.");
+        }
+        if (ast.getThenStatements().isEmpty()) {
+            throw new RuntimeException("THEN block cannot have an empty body");
+        }
+        // Evaluate the else statements inside a new scope:
+        scope = new Scope(scope);
+        ast.getElseStatements().forEach(this::visit);
+        scope = scope.getParent();
+        // Evaluate the then statements inside a new scope:
+        scope = new Scope(scope);
+        ast.getThenStatements().forEach(this::visit);
+        scope = scope.getParent();
+
+        return null;
     }
 
     @Override
     public Void visit(Ast.Statement.Switch ast) {
-        throw new UnsupportedOperationException();  // TODO
+        // Visit the condition:
+        visit(ast.getCondition());
+        // Loop through each case statement, checking for exception conditions, visiting the value, and then visiting the case itself:
+        List<Ast.Statement.Case> caseList = ast.getCases();
+        for (int i = 0; i < caseList.size(); i++) {
+            if (caseList.get(i).getValue().isPresent()) {
+                if (i == caseList.size() - 1) {
+                    throw new RuntimeException("Default case cannot specify a value.");
+                }
+                visit(caseList.get(i).getValue().get());
+                if (!caseList.get(i).getValue().get().getType().equals(ast.getCondition().getType())) {
+                    throw new RuntimeException("Condition and case value must match in a switch statement.");
+                }
+            }
+            visit(caseList.get(i));
+        }
+
+        return null;
     }
 
     @Override
     public Void visit(Ast.Statement.Case ast) {
-        throw new UnsupportedOperationException();  // TODO
+        // Visit each of the case's statements in a new scope:
+        scope = new Scope(scope);
+        ast.getStatements().forEach(this::visit);
+        scope = scope.getParent();
+
+        return null;
     }
 
     @Override
     public Void visit(Ast.Statement.While ast) {
-        throw new UnsupportedOperationException();  // TODO
+        // Visit the condition:
+        visit(ast.getCondition());
+        // Check the exception condition:
+        if (!ast.getCondition().getType().equals(Environment.Type.BOOLEAN)) {
+            throw new RuntimeException("Invalid condition in WHILE statement.");
+        }
+        // Visit each of the block's statements in a new scope:
+        scope = new Scope(scope);
+        ast.getStatements().forEach(this::visit);
+        scope = scope.getParent();
+
+        return null;
     }
 
     @Override
     public Void visit(Ast.Statement.Return ast) {
-        requireAssignable(Environment.getType(function.getReturnTypeName().orElse("Nil")), ast.getValue().getType());
+        // Visit the return value:
+        visit(ast.getValue());
+        // Check that the given value is assignable to the function's return type (stored in funcRet):
+        requireAssignable(funcRet, ast.getValue().getType());
         return null;
     }
 
     @Override
     public Void visit(Ast.Expression.Literal ast) {
-        throw new UnsupportedOperationException();  // TODO
+        // Check for integer exception condition (byte array has length greater than 4 bytes (32 bits))
+        if (ast.getLiteral() instanceof BigInteger) {
+            if (((BigInteger) ast.getLiteral()).toByteArray().length > 4) {
+                throw new RuntimeException("Integer value will overflow.");
+            }
+            ast.setType(Environment.Type.INTEGER);
+        }
+        // Check for decimal exception condition (overflow occurs when casting from BigDecimal to double)
+        else if (ast.getLiteral() instanceof BigDecimal) {
+            double doubleVal = ((BigDecimal) ast.getLiteral()).doubleValue();
+            if (doubleVal == Double.POSITIVE_INFINITY || doubleVal == Double.NEGATIVE_INFINITY) {
+                throw new RuntimeException("Decimal value will overflow.");
+            }
+            ast.setType(Environment.Type.DECIMAL);
+        }
+        else if (ast.getLiteral() instanceof Boolean) {
+            ast.setType(Environment.Type.BOOLEAN);
+        }
+        else if (ast.getLiteral() instanceof Character) {
+            ast.setType(Environment.Type.CHARACTER);
+        }
+        else if (ast.getLiteral() instanceof String) {
+            ast.setType(Environment.Type.STRING);
+        }
+        else {
+            ast.setType(Environment.Type.NIL);
+        }
+
+        return null;
     }
 
     @Override
     public Void visit(Ast.Expression.Group ast) {
-        throw new UnsupportedOperationException();  // TODO
+        // Check the exception condition:
+        if (!(ast.getExpression() instanceof Ast.Expression.Binary)) {
+            throw new RuntimeException("The grouped expression is not binary.");
+        }
+        // Visit the contained expression:
+        visit(ast.getExpression());
+        // Set the type of the AST based on the type of the contained expression:
+        ast.setType(ast.getExpression().getType());
+
+        return null;
     }
 
     @Override
     public Void visit(Ast.Expression.Binary ast) {
-        throw new UnsupportedOperationException();  // TODO
+        switch (ast.getOperator()) {
+            case "&&":
+            case"||": {
+                // Visit the left and right sides of the expression:
+                visit(ast.getLeft());
+                visit(ast.getRight());
+                // Assign a type to the AST:
+                if (ast.getLeft().getType().equals(Environment.Type.BOOLEAN) && ast.getRight().getType().equals(Environment.Type.BOOLEAN)) {
+                    ast.setType(Environment.Type.BOOLEAN);
+                }
+                else {
+                    throw new RuntimeException("Expected boolean values on both sides of the binary expression.");
+                }
+                break;
+            }
+            case "<":
+            case ">":
+            case "==":
+            case "!=": {
+                // Visit the left and right sides of the expression:
+                visit(ast.getLeft());
+                visit(ast.getRight());
+                // Assign a type to the AST:
+                if (ast.getLeft().getType().getScope().getParent().equals(Environment.Type.COMPARABLE.getScope()) &&
+                        ast.getRight().getType().getScope().getParent().equals(Environment.Type.COMPARABLE.getScope())) {
+                    if (ast.getLeft().getType().equals(ast.getRight().getType())) {
+                        ast.setType(Environment.Type.BOOLEAN);
+                    }
+                    else {
+                        throw new RuntimeException("Left and right sides of equality statement must match.");
+                    }
+                }
+                else {
+                    throw new RuntimeException("Both sides of equality statement must be of type COMPARABLE.");
+                }
+                break;
+            }
+            case "+": {
+                // Visit the left and right sides of the expression:
+                visit(ast.getLeft());
+                visit(ast.getRight());
+                // Assign a type to the AST:
+                if (ast.getLeft().getType().equals(Environment.Type.STRING) || ast.getRight().getType().equals(Environment.Type.STRING)) {
+                    ast.setType(Environment.Type.STRING);
+                }
+                else checkTypesMatch(ast);
+                break;
+            }
+            case "-":
+            case "*":
+            case "/": {
+                // Visit the left and right sides of the expression:
+                visit(ast.getLeft());
+                visit(ast.getRight());
+                // Assign a type to the AST:
+                checkTypesMatch(ast);
+                break;
+            }
+            case "^": {
+                // Visit the left and right sides of the expression:
+                visit(ast.getLeft());
+                visit(ast.getRight());
+                // Assign a type to the AST:
+                if ((ast.getLeft().getType().equals(Environment.Type.INTEGER) || ast.getLeft().getType().equals(Environment.Type.DECIMAL)) && ast.getRight().getType().equals(Environment.Type.INTEGER)) {
+                    ast.setType(ast.getLeft().getType());
+                }
+                else {
+                    throw new RuntimeException("Invalid binary expression.");
+                }
+                break;
+            }
+        }
+
+        return null;
     }
 
     @Override
     public Void visit(Ast.Expression.Access ast) {
-        throw new UnsupportedOperationException();  // TODO
+        // Check the exception condition:
+        if (ast.getOffset().isPresent()) {
+            visit(ast.getOffset().get());
+            if (ast.getOffset().get().getType().equals(Environment.Type.INTEGER)) {
+                throw new RuntimeException("Offset must be an integer value.");
+            }
+        }
+        // Set the variable of the expression:
+        ast.setVariable(scope.lookupVariable(ast.getName()));
+
+        return null;
     }
 
     @Override
     public Void visit(Ast.Expression.Function ast) {
-        throw new UnsupportedOperationException();  // TODO
+        // Set the function of the expression:
+        ast.setFunction(scope.lookupFunction(ast.getName(), ast.getArguments().size()));
+        // Check that the argument types are assignable to the parameter types:
+        List<Ast.Expression> args = ast.getArguments();
+        List<Environment.Type> params = ast.getFunction().getParameterTypes();
+        for (int i = 0; i < args.size(); i++) {
+            visit(args.get(i)); // Visit each argument
+            requireAssignable(params.get(i), args.get(i).getType());
+        }
+
+        return null;
     }
 
     @Override
     public Void visit(Ast.Expression.PlcList ast) {
-        throw new UnsupportedOperationException();  // TODO
+        // Visit each value in the list and check whether its type is assignable to the type of the list itself:
+        List<Ast.Expression> values = ast.getValues();
+        for (Ast.Expression val : values) {
+            visit(val);
+            requireAssignable(ast.getType(), val.getType());
+        }
+
+        return null;
     }
 
     public static void requireAssignable(Environment.Type target, Environment.Type type) {
-        if (target.getName().equals(type.getName())) {
+        if (target.equals(type)) {
             return;
         }
-        if (target.getName().equals("Any")) {
+        if (target.equals(Environment.Type.ANY)) {
             return;
         }
-        if (target.getName().equals("Comparable")) {
+        if (target.equals(Environment.Type.COMPARABLE)) {
             if (type.getName().equals("Integer") || type.getName().equals("Decimal") || type.getName().equals("Character")
                     || type.getName().equals("String")) {
                 return;
             }
         }
-        throw new RuntimeException("Invalid assignment! Attempting to assign " + type.getName() + " to a " + type.getName() + " variable.");
+        throw new RuntimeException("Invalid assignment: attempting to assign " + type.getName() + " to a " + target.getName() + " variable.");
+    }
+
+    private void checkTypesMatch(Ast.Expression.Binary ast) {   // Used in visit(Ast.Expression.Binary) -- checks if rhs and lhs types match and sets the ast type accordingly
+        if (ast.getLeft().getType().equals(Environment.Type.INTEGER) && ast.getRight().getType().equals(Environment.Type.INTEGER)) {
+            ast.setType(Environment.Type.INTEGER);
+        }
+        else if (ast.getLeft().getType().equals(Environment.Type.DECIMAL) && ast.getRight().getType().equals(Environment.Type.DECIMAL)) {
+            ast.setType(Environment.Type.DECIMAL);
+        }
+        else {
+            throw new RuntimeException("Invalid binary expression.");
+        }
     }
 }
+
